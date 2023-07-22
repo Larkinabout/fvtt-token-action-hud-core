b8b31050b1157773516727b1339cec0412563011
typhonrt <support@typhonrt.org>
2023-07-23 10:02:06 +1200

    remove unnecessary multi Rollup plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from '@rollup/plugin-commonjs'
-import multi from '@rollup/plugin-multi-entry'
 import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 
@@ -8,14 +7,15 @@ export default [
         input: 'scripts/init.js',
         plugins: [
             commonjs(),
-            resolve({ browser: true }),
-            terser({ keep_classnames: true, keep_fnames: true }),
-            multi()
+            resolve({ browser: true })
         ],
         output: {
             format: 'esm',
             file: 'dist/token-action-hud-core.min.js',
             generatedCode: { constBindings: true },
+            plugins: [
+                terser({ keep_classnames: true, keep_fnames: true })
+            ],
             sourcemap: true
         }
     }
