76e126857e27a9afd99722e7ed049fb47f052715
typhonrt <support@typhonrt.org>
2023-07-23 09:56:25 +1200

    Refactor Rollup build for single file entry point. Output to dist / not checked in. Update GH action.

--- rollup.config.js
+++ rollup.config.js
@@ -5,22 +5,17 @@ import terser from '@rollup/plugin-terser'
 
 export default [
     {
-        input: {
-            include: [
-                'scripts/*.js',
-                'scripts/*/*.js'
-            ],
-            exclude: ['scripts/token-action-hud-core.min.js']
-        },
-        output: {
-            format: 'esm',
-            file: 'scripts/token-action-hud-core.min.js'
-        },
+        input: 'scripts/init.js',
         plugins: [
             commonjs(),
             resolve({ browser: true }),
             terser({ keep_classnames: true, keep_fnames: true }),
             multi()
-        ]
+        ],
+        output: {
+            format: 'esm',
+            file: 'dist/token-action-hud-core.min.js',
+            generatedCode: { constBindings: true }
+        }
     }
 ]
