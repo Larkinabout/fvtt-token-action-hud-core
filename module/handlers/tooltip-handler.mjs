4f66d0b209742e837b2afbb433d38591bb1498a1
Russell Larkin <russell.larkin@gmail.com>
2026-09-21 23:55:50 +0100

    #382

--- module/handlers/tooltip-handler.mjs
+++ module/handlers/tooltip-handler.mjs
@@ -4,19 +4,22 @@ import { Utils } from "../core/utils.mjs";
  * Get tooltip based on module setting.
  * @param {object|string} tooltip
  * @param {string} name
- * @returns {object} Tooltip
+ * @returns {object|null} Tooltip
  */
 export function getTooltip(tooltip, name) {
   const setting = game.tokenActionHud?.setting?.tooltips ?? Utils.getSetting("tooltips");
 
   if (setting === "none") return null;
-  if (setting === "nameOnly" || !tooltip) return name;
 
-  if (typeof tooltip !== "object") {
+  if (!tooltip || typeof tooltip !== "object") {
     tooltip = { content: tooltip };
   }
 
-  if (!tooltip.content.includes("tah-tooltip-wrapper")) {
+  const hasContent = tooltip.content && typeof tooltip.content === "string";
+
+  if (setting === "nameOnly" || !hasContent) {
+    tooltip = { content: name, direction: tooltip.direction };
+  } else if (!tooltip.content.includes("tah-tooltip-wrapper")) {
     tooltip.content = `<div class="tah-tooltip-wrapper">${tooltip.content}</div>`;
   }
 
@@ -24,9 +27,10 @@ export function getTooltip(tooltip, name) {
   if (tooltip.direction) {
     direction = tooltip.direction;
   } else {
-    const style = game.tokenActionHud.systemManager.styles[game.tokenActionHud.setting.style];
+    const styles = game.tokenActionHud.systemManager.styles;
+    const style = styles[game.tokenActionHud.setting.style] ?? styles.foundryVTT;
 
-    if (["center-right", "right"].includes(style.dockPosition)) {
+    if (["center-right", "right"].includes(style?.dockPosition)) {
       direction = "LEFT";
     }
   }
